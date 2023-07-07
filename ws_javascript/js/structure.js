e5e99130bbb15adb81e5a9cc759ddc89bf37966f
alin <alinmansita@gmail.com>
2023-07-07 16:54:43 +0200

    tp js

---
+++ ws_javascript/js/structure.js
@@ -0,0 +1,7 @@
+// il faut changer les nom de id ici et aussi dans html
+const myCarouselElement = document.querySelector('#myCarousel1')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 100,
+    touch: false
+})
+
